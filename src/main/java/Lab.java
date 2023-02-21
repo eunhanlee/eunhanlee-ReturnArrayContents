b173fa9955d66eed72eb99fcf14af8ee90d17566
Eunhan Lee <eunhanlee58@gmail.com>
2023-02-21 22:59:01 +0000

    Return Array Contents

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,12 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for(int val:nums){
+            sb.append(String.valueOf(val));
+            sb.append(" ");
+        }
+        
+        return sb.toString().trim();
     }
 }
